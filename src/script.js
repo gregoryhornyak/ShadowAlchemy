5ff0178e7ecb0318d2c71eadf22e4c04e657dedb
Gergely Hornyak <gergely2125@gmail.com>
2024-07-19 11:31:53 +0200

    draw background

--- src/script.js
+++ src/script.js
@@ -1,16 +1,22 @@
 const canvas = document.getElementById('game_canvas');
 const ctx = canvas.getContext('2d');
 
+var wall = new Image();
+wall.src = "https://www.svgrepo.com/show/178610/brick-wall-wall.svg";
+wall.onload = function() {
+    ctx.drawImage(wall,0,0,300,300);
+}
+
 var player = new Image();
 player.src = "https://www.svgrepo.com/show/49407/scientist.svg";
 player.onload = function() {
-    ctx.drawImage(player,0,2);
+    ctx.drawImage(player,0,0,300,300);
 }
 
 var desk = new Image();
 desk.src = "https://www.svgrepo.com/show/230630/desk.svg";
 desk.onload = function() {
-    ctx.drawImage(desk,-100,350,1000,1000);
+    ctx.drawImage(desk,0,350,300,300);
 }
 
 function game_loop() {
