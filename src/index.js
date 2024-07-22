379005428fc1a4d5600b89892e6a6573887e3db9
Gergely Hornyak <gergely2125@gmail.com>
2024-07-22 22:34:17 +0200

    separate index js file, imported by index.html

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { Game } from "./game.js";
+
+const canvas = document.getElementById('game_canvas');
+const ctx = canvas.getContext('2d');
+
+const CANVAS_WIDTH = 1000;
+const CANVAS_HEIGHT = CANVAS_WIDTH*0.5625;
+
+let game = new Game(CANVAS_WIDTH, CANVAS_HEIGHT);
+
+function game_loop(timestamp) {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    game.update();
+    game.draw();
+    requestAnimationFrame(game_loop);
+}
+
+requestAnimationFrame(game_loop)
