ff656c62890c29f63e18bdd0385d26308e9d8495
Gergely Hornyak <gergely2125@gmail.com>
2024-07-23 09:01:36 +0200

    track mouse position

--- src/input_handle.js
+++ src/input_handle.js
@@ -1,7 +1,21 @@
+function getPos(e){
+    var x=e.clientX;
+    var y=e.clientY;
+    var cursor = "";
+    var over_player = false;
+    cursor="Your Mouse Position Is : " + x + " and " + y ;
+    // 484, 314 - 189, 14
+    if ((x < 484 && x > 189) && (y < 314 && y > 14)) {
+        cursor="Over the player";
+        over_player = true;
+    }
+    console.log(cursor)
+    return over_player;
+}
 export default class InputHandler {
     constructor(player, game) {
-        document.addEventListener("mouseover", (event) => {
-            
+        document.addEventListener("mousemove", (event) => {
+            getPos(event)
         })
     }
 }
