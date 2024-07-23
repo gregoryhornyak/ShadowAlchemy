c3a81346432bf757703e6ec938281cba0db9ac7b
Gergely Hornyak <gergely2125@gmail.com>
2024-07-23 10:41:41 +0200

    target_tile animation assist

--- src/player.js
+++ src/player.js
@@ -13,6 +13,11 @@ export default class Player {
             x: 0,
             y: 0
         };
+        // for smooth moving, using target tiles to help the animation.
+        this.target_tile = {
+            x: 0,
+            y: 0
+        };
     }
     draw(ctx) {
         //ctx.drawImage(this.texture,this.position.x,this.position.x,300*this.size,300*this.size);  
@@ -22,27 +27,23 @@ export default class Player {
         ctx.fill();
     }
     update(deltaTime) {
+        if (target_tile.x != this.position.x || target_tile.y != this.position.y)
         this.position.x += this.speed;
         if (this.position.x < 0) this.position.x = 0;
         if (this.position.x + this.width > this.gameWidth) {
             this.position.x = this.gameWidth - this.width;
         }
     }
-    move_from_current_tile_to(direction) {
-        if (direction == "left") {
-            
-        }
-    }
     move_left() {
-        target_tile = this.move_from_current_tile_to("left");
+        target_tile.x = this.position.x - unit;
     }
     move_right() {
-        target_tile = this.move_from_current_tile_to("right");
+        target_tile.x = this.position.x + unit;
     }
     move_up() {
-        target_tile = this.move_from_current_tile_to("up");
+        target_tile.y = this.position.y - unit;
     }
     move_down() {
-        target_tile = this.move_from_current_tile_to("down");
+        target_tile.y = this.position.y + unit;
     }
 }
