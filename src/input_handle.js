87dd7a2831eb7ac2dcbabacc462c34dbe90a1e3b
Gergely Hornyak <gergely2125@gmail.com>
2024-07-23 20:05:26 +0200

    remove unnecessary console log

--- src/input_handle.js
+++ src/input_handle.js
@@ -4,11 +4,6 @@ function getPos(e){
     var cursor = "";
     var over_player = false;
     cursor="Your Mouse Position Is : " + x + " and " + y ;
-    // 484, 314 - 189, 14
-    if ((x < 484 && x > 189) && (y < 314 && y > 14)) {
-        cursor="Over the player";
-        over_player = true;
-    }
     console.log(cursor)
     return over_player;
 }
@@ -32,9 +27,8 @@ export default class InputHandler {
             }
             if (event.repeat) {
                 console.log("repeated")
-                return
             }
-            console.log(event.key)
+            //console.log(event.key)
         });
     }
 }
