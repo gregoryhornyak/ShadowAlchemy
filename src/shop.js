bd4f1ec5a240602aac585dee89e7c0e3954fc8be
Gergely Hornyak <gergely2125@gmail.com>
2024-07-23 13:30:58 +0200

    assets in shop added

--- src/shop.js
+++ src/shop.js
@@ -1,4 +1,4 @@
-export default class Player {
+export default class Shop {
     constructor(game) {
         this.grid_width = 10;
         this.grid_height = 6;
@@ -13,7 +13,7 @@ export default class Player {
             for (let j = 0; j < this.grid_height; j++) {
                 ctx.fillStyle = "black";
                 ctx.fillRect((i)*square_size, (j)*square_size, square_size-2, square_size-2);
-                ctx.fillStyle = "yellow";
+                ctx.fillStyle = "PapayaWhip";
                 ctx.fillRect((i)*square_size+4, (j)*square_size+4, square_size-8, square_size-8);
             }
         }        
@@ -23,5 +23,11 @@ export default class Player {
         //desk
         ctx.fillStyle = "brown";
         ctx.fillRect(0,3*this.square_size,6*this.square_size,1*this.square_size);
+        //shelf
+        ctx.fillStyle = "LightSalmon";
+        ctx.fillRect(1*this.square_size,0*this.square_size,2*this.square_size,1*this.square_size);
+        //window_shelf
+        ctx.fillStyle = "PaleGoldenrod";
+        ctx.fillRect(0*this.square_size,1*this.square_size,1*this.square_size,2*this.square_size);
     }
 }
