cee9654fb3a1ab92e795c5224bd49d0592c38c40
Gergely Hornyak <gergely2125@gmail.com>
2024-07-23 20:06:37 +0200

    constant colection

---
+++ src/constants.js
@@ -0,0 +1,27 @@
+export default class Constants {
+    constructor() {
+        this.CANVAS_WIDTH = 1000;
+        this.CANVAS_HEIGHT = 700;
+        this.GAMESTATES = {
+            PAUSED: 0,
+            RUNNING: 1,
+            MENU: 2,
+            GAMEOVER: 3
+        };
+        this.TILE_SIZE = 100;
+    }
+}
+
+/*
+
+export const CANVAS_WIDTH = 1000;
+export const CANVAS_HEIGHT = 700;
+export const GAMESTATES = {
+    PAUSED: 0,
+    RUNNING: 1,
+    MENU: 2,
+    GAMEOVER: 3
+};
+export const TILE_SIZE = 100;
+
+*/
