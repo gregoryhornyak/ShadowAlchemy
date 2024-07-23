e31dd380dfe5ddaa7c0fd786d03d81bd558df403
Gergely Hornyak <gergely2125@gmail.com>
2024-07-23 13:30:41 +0200

    snap to grid movement

--- src/input_handle.js
+++ src/input_handle.js
@@ -16,6 +16,25 @@ export default class InputHandler {
     constructor(player, game) {
         document.addEventListener("mousemove", (event) => {
             getPos(event)
-        })
+        });
+        document.addEventListener("keydown", event => {
+            if (event.key == "ArrowLeft") {
+                player.move_left()
+            }
+            if (event.key == "ArrowRight") {
+                player.move_right()
+            }
+            if (event.key == "ArrowDown") {
+                player.move_up()
+            }
+            if (event.key == "ArrowUp") {
+                player.move_down()
+            }
+            if (event.repeat) {
+                console.log("repeated")
+                return
+            }
+            console.log(event.key)
+        });
     }
 }
