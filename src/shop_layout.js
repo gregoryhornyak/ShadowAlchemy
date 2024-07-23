4d035de219dd4a45dd3f81072dabe331906b5110
Gergely Hornyak <gergely2125@gmail.com>
2024-07-23 17:38:51 +0200

    layout of the shop as a map

---
+++ src/shop_layout.js
@@ -0,0 +1,17 @@
+export default class ShopLayout {
+    constructor() {
+        // [B]locked, [S]helf, [W]all, [A]vailable, [G]ate, [D]esk 
+        this.layout = [
+            "W","W","W","W","W","W","W","W","W","W",
+            "W","W","W","S","S","A","G","A","A","W",
+            "W","S","A","A","A","A","W","A","S","W",
+            "W","D","D","D","D","D","W","A","S","W",
+            "W","A","A","A","A","A","W","A","S","W",
+            "W","A","A","A","A","A","W","W","W","W"
+        ];
+
+    }
+    get_tile(x,y) {
+        return this.layout[10*y+x]
+    }
+}
