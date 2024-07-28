f9f4dd60cff7383f2068adf7dc6cec905c2ad1bb
Gergely Hornyak <gergely2125@gmail.com>
2024-07-28 22:37:24 +0200

    instanciate garden and shop objects

--- src/scene_handler.js
+++ src/scene_handler.js
@@ -1,7 +1,13 @@
+import Shop from "./shop.js";
+import Garden from "./garden.js";
+
 export default class SceneHandler {
     constructor(game,starting_scene) {
         this.game = game;
-        this.current_scene = starting_scene // starting scene
+        this.garden = new Garden(game);
+        this.shop = new Shop(game);
+        // change current scene to object, then access ".name"
+        this.current_scene = starting_scene // starting scene: shop
     }
     change_scene(target_scene) {
         console.log(`Change scene to ${target_scene}.`);
@@ -9,15 +15,15 @@ export default class SceneHandler {
         return 0;
     }
     render_scene(ctx) {
-        console.log(`current scene: ${this.current_scene}`)
+        console.log(`Current scene: ${this.current_scene}`)
         if (this.current_scene == "shop") {
             console.log("rendering shop");
-            this.game.shop.draw(ctx);
+            this.shop.draw(ctx);
             return 0;
         }
         if (this.current_scene == "garden") {
             console.log("rendering garden");
-            this.game.garden.draw(ctx);
+            this.garden.draw(ctx);
             return 0;
         }
     }
