c8932121bcfb8a7f06a9b5eb61da7a0b1c755fa9
Gergely Hornyak <gergely2125@gmail.com>
2024-07-28 21:42:36 +0200

    is obtained, draw on player

--- src/potion.js
+++ src/potion.js
@@ -1,5 +1,6 @@
 export default class Potion {
     constructor(game) {//,name,type,texture) {
+        this.game = game;
         this.name = "Health-Regen";
         this.type = "red";
         this.texture = new Image();
@@ -8,8 +9,18 @@ export default class Potion {
             x: 100,
             y: 100
         };
+        this.is_obtained = false;
     }
     draw(ctx) {
-        ctx.drawImage(this.texture,this.position.x+20,this.position.y+20,100-40,100-40);
+        if (this.is_obtained == false) {
+            ctx.drawImage(this.texture,this.position.x+20,this.position.y+20,100-40,100-40);
+        }
+        else { // if obtained
+            ctx.drawImage(this.texture,
+                this.game.player.position.x+this.game.player.size-40,
+                this.game.player.position.y+this.game.player.size-40,
+                40,
+                40);
+        }
     }
 }
