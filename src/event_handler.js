0c052cfceee82c2cbeabc51cf4ba9b6e0dbe33f7
Gergely Hornyak <gergely2125@gmail.com>
2024-07-28 20:45:58 +0200

    check if player is able to interact with shelf

--- src/event_handler.js
+++ src/event_handler.js
@@ -28,8 +28,22 @@ export default class EventHandler {
         }
         else {return 0}
     }
+    check_player_interaction() {
+        if (this.game.player.position.x == 100 && this.game.player.position.y == 200) {
+            if (this.game.sceneHandler.current_scene == "shop") {
+                console.log("enable selection")
+                this.game.selection.enable();
+            }
+        }
+        else {
+            if (this.game.sceneHandler.current_scene == "shop") {
+                this.game.selection.disable();
+            }
+        }
+    }
     update() {
         this.check_enter_garden();
         this.check_enter_shop();
+        this.check_player_interaction();
     }
 }
